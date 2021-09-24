68615fcaac1d63f41afdc2255a30789c7dbcb41a
Zikri KN <zikrikn@student.telkomuniversity.ac.id>
2021-09-24 22:19:53 +0700

    testing

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -8,19 +9,66 @@ import (
 
 func main() {
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-			"name": "Zikri KN is Awesome",
-		})
-	})
 
-	r.GET("/hello", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "hello world is hmm",
-			"name": "your life",
-		})
-	})
+	//making a lot of varian of endpoints
+	r.GET("/", rootHandler) //GET untuk mengambil atau meminta data atau consume
+	r.GET("/hello", helloHandler)
+	r.GET("movies/:id/:title", booksHandler)
+	r.GET("/query", queryHandler)
+	r.POST("/movies", postBooksHandler) //POST untuk meng-create sesuatu
 
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run("localhost:8888") // nanti bisa di Run() aja untuk di deploy di website
+	//untuk local pake localhost:8080 dulu
 }
+
+func rootHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "pong",
+		"name":    "Zikri KN is Awesome",
+	})
+}
+
+func helloHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "ping",
+		"name":    "Zikri KN is Nice",
+	})
+}
+
+func booksHandler(c *gin.Context) {
+	id := c.Param("id")
+	title := c.Param("title")
+
+	c.JSON(http.StatusOK, gin.H{"id": id, "title": title})
+}
+
+func queryHandler(c *gin.Context) {
+	title := c.Query("title")
+	price := c.Query("price")
+
+	c.JSON(http.StatusOK, gin.H{"title": title, "price": price})
+}
+
+type moviesInput struct {
+	Title string
+	Price int `json:"price"` 
+	SubTitle string `json:"sub_title"` //untuk mendeklarasikan di variabel json
+}
+
+func postBooksHandler(c *gin.Context) {
+	//title, ranting
+	var movieInput moviesInput
+
+	err := c.ShouldBindJSON(&movieInput)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"title": movieInput.Title,
+		"price": movieInput.Price,
+		"sub_title": movieInput.SubTitle,
+	})
+}
+
+//3. HTTPS POST & VALIDATION
