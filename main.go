23e0a71f34904a67cc9f0b47bb27a7411b4ba688
Zikri KN <zikrikn@student.telkomuniversity.ac.id>
2021-09-24 13:08:17 +0700

    update1

--- main.go
+++ main.go
@@ -22,5 +22,5 @@ func main() {
 		})
 	})
 
-	r.Run("https://zikrikn.herokuapp.com") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
